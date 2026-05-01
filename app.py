078f0670a2f416cbbc9f8c6f3925239faf03086b
rasta-clinic <rousta_parvin@yahoo.com>
2026-05-01 19:05:27 +0330

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,8 @@ def therapists():
     return render_template('therapists.html')
 
 # -------------------- اجرا --------------------
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
